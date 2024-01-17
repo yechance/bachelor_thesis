8896bea8a80f53e72f630a4340d8e9668bb46845
Yechan Kim <cks15969@gmail.com>
2024-01-17 20:11:02 +0100

    implemented the message sending logic on the client side.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,50 +1,48 @@
 #[macro_use]
 extern crate log;
+extern crate common;
+mod prelude {
+    pub use std::env;
+    pub use std::collections::HashMap;
+    pub use std::time::{Duration, Instant};
 
-use quiche;
-use ring::rand::*;
-use std::time::{Duration, Instant};
-use serde::{Serialize, Deserialize};
-// use log::{debug, error, trace, warn, info};
-use mio::net::UdpSocket;
-const CLIENT_ADDR : &str = "127.0.0.1:3000";
-const SERVER_ADDR : &str = "127.0.0.1:8000";
-const MAX_DATAGRAM_SIZE: usize = 1350;
+    pub use uuid::Uuid;
+    pub use ring::rand::*;
 
-struct MessageBody {
-    pub req_send_timestamp : Instant,
-    pub req_receive_timestamp : Instant,
-    pub res_send_timestamp : Instant,
-    pub res_receive_timestamp : Instant,
-    pub body : Vec<u8>
+    pub use quiche;
+    pub use mio::net::UdpSocket;
+    pub use common::*;
 }
+use crate::prelude::*;
 
-impl MessageBody {
+// #[derive(Serialize, Deserialize)]
 
-}
 fn main() {
-    // TODO : Data to be sent (for testing)
-    let data = [1;65535];
-    // Buffer & Setup the event loop
-    let peer_addr = CLIENT_ADDR.parse().unwrap();
-    let mut buf = [0;65535];
-    let mut out = [0; MAX_DATAGRAM_SIZE];
+    // args [path, message_size]
+    let args: Vec<String> = env::args().collect();
+    let message_size : usize = args[1].parse().unwrap();
+    let mut sent_bytes = 0;
+    let mut records : HashMap<Uuid, &mut Record> = HashMap::new();
 
+    // Buffer & Setup the event loop
+    // 연결 할 서버의 주소를 받아오고 버퍼들을 준비한다.
+    let peer_addr = SERVER_ADDR.parse().unwrap();
+    let mut buf = [0;65535];
+    let mut out = [0;MAX_DATAGRAM_SIZE];
+    // 비동기 이벤트 루프를 만들기 위한 세팅
     let mut poll = mio::Poll::new().unwrap();
     let mut events = mio::Events::with_capacity(1024);
 
     // Create the UDP socket backing the QUIC connection, and register it with the event loop.
+    // 해당 서버 소켓을 poll에 등록을 한다.
     let mut socket = UdpSocket::bind(peer_addr).unwrap();
     poll.registry()
-        .register(&mut socket, mio::Token(0), mio::Interest::READABLE)
+        .register(&mut socket, mio::Token(0), mio::Interest::READABLE | mio::Interest::WRITABLE)
         .unwrap();
 
     // Create the configuration for the QUIC connection.
+    // QUICHE 설정
     let mut config = quiche::Config::new(quiche::PROTOCOL_VERSION).unwrap();
-
-    // *CAUTION* : this should not be set to 'false' in production!
-    config.verify_peer(false);
-
     config.set_application_protos(&[
             b"hq-interop",
             b"hq-29",
@@ -52,7 +50,6 @@ fn main() {
             b"hq-27",
             b"http/0.9",
             ]).unwrap();
-
     config.set_max_idle_timeout(5000);
     config.set_max_recv_udp_payload_size(MAX_DATAGRAM_SIZE);
     config.set_max_send_udp_payload_size(MAX_DATAGRAM_SIZE);
@@ -65,12 +62,13 @@ fn main() {
     config.set_cc_algorithm_name("bbr2").unwrap(); // BBR2 is the CCA
 
     // Generate a random source connection ID for the connection.
+    // 연결용 아이디 생성
     let mut scid = [0; quiche::MAX_CONN_ID_LEN];
     SystemRandom::new().fill(&mut scid[..]).unwrap();
     let scid = quiche::ConnectionId::from_ref(&scid);
 
     // Get local address.
-    let local_addr = socket.local_addr().unwrap();
+    let local_addr = CLIENT_ADDR.parse().unwrap();
     let server_name = "server";
 
     // Create a QUIC connection and initiate handshake.
@@ -96,20 +94,16 @@ fn main() {
 
     debug!("written {}", write);
 
-    let req_start = std::time::Instant::now();
-
-    let mut req_sent = false;
-
-    // TODO : READ UDP packets
+    // 이벤트 루프 : 패킷 송수신
     loop {
         poll.poll(&mut events, conn.timeout()).unwrap();
 
         // Read incoming UDP packets from the socket and feed them to quiche,
         // until there are no more packets to read.
         'read: loop {
-            // If the event loop reported no events, it means that the timeout
-            // has expired, so handle it without attempting to read packets. We
-            // will then proceed with the send loop.
+            // If the event loop reported no events, it means that the timeout has expired,
+            // so handle it without attempting to read packets.
+            // We will then proceed with the send loop.
             if events.is_empty() {
                 debug!("timed out");
 
@@ -117,17 +111,15 @@ fn main() {
                 break 'read;
             }
 
+            // 패킷 수신
             let (len, from) = match socket.recv_from(&mut buf) {
                 Ok(v) => v,
-
                 Err(e) => {
-                    // There are no more UDP packets to read, so end the read
-                    // loop.
+                    // There are no more UDP packets to read, so end the read loop.
                     if e.kind() == std::io::ErrorKind::WouldBlock {
                         debug!("recv() would block");
                         break 'read;
                     }
-
                     panic!("recv() failed: {:?}", e);
                 },
             };
@@ -142,13 +134,18 @@ fn main() {
             // Process potentially coalesced packets.
             let read = match conn.recv(&mut buf[..len], recv_info) {
                 Ok(v) => v,
-
                 Err(e) => {
                     error!("recv failed: {:?}", e);
                     continue 'read;
                 },
             };
+            // Deserialize
+            let ack_packet = PacketBody::deserialize(buf[..len]);
 
+            // Calculate the actual rtt
+            let mut record = records.get(ack_packet.id).unwrap();
+            record.ack_timestamp = Instant::now();
+            record.actual_rtt = record.send_timestamp.elapsed();
             debug!("processed {} bytes", read);
         }
 
@@ -158,7 +155,7 @@ fn main() {
             info!("connection closed, {:?}", conn.stats());
             break;
         }
-
+        {
         // // Send an HTTP request as soon as the connection is established.
         // if conn.is_established() && !req_sent {
         //     // info!("sending HTTP request for {}", url.path());
@@ -200,29 +197,47 @@ fn main() {
         //         }
         //     }
         // }
+        }
 
-        // TODO : Send packets
+        // Generate outgoing QUIC packets and send them on the UDP socket,
+        // until quiche reports that there are no more packets to be sent.
+        'write: loop {
+            // Calculate the size of the dgram packet
+            if sent_bytes >= message_size {
+                break 'write;
+            }
+            let mut packet_size = MAX_DATAGRAM_SIZE - UUID_SIZE;
+            if sent_bytes + packet_size > message_size {
+                packet_size = message_size - sent_bytes;
+            }
+            // Create packet and serialize it.
+            let packet_body = PacketBody::new(packet_size);
+            let mut serialized : Vec<u8> = packet_body.serialize();
+            // record
+            let mut record: Record = Record{
+                packet_id : packet_body.id,
+                actual_rtt : Duration::new(0,0),
+                send_timestamp : Instant::now(),
+                ack_timestamp : Instant::now(),
+                message_size : message_size,
+                // path_stats : conn.path_stats().clone(),
+            };
+            records.insert(record.packet_id, &mut record);
 
-        // Generate outgoing QUIC packets and send them on the UDP socket, until
-        // quiche reports that there are no more packets to be sent.
-        loop {
-            let (write, send_info) = match conn.send(&mut out) {
+            let (write, send_info) = match conn.send(serialized.as_mut_slice()) {
                 Ok(v) => v,
-
                 Err(quiche::Error::Done) => {
                     debug!("done writing");
-                    break;
+                    break 'write;
                 },
-
                 Err(e) => {
                     error!("send failed: {:?}", e);
 
                     conn.close(false, 0x1, b"fail").ok();
-                    break;
+                    break 'write;
                 },
             };
-
-            if let Err(e) = socket.send_to(&out[..write], send_info.to) {
+            if let Err(e) = socket.send_to(&serialized[..write], send_info.to) {
                 if e.kind() == std::io::ErrorKind::WouldBlock {
                     debug!("send() would block");
                     break;
@@ -239,6 +254,10 @@ fn main() {
             break;
         }
     }
+
+    // Draw Data
+    // or
+    // Write the data
 }
 
 fn hex_dump(buf: &[u8]) -> String {
